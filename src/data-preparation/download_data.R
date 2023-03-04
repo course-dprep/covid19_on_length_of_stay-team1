d4d7a8e91a691aa0d777b36bed4d9d3427fb5f45
Jonas Klein <jonasklein2001@gmail.com>
2023-03-04 21:07:50 +0100

    Changed files in line with makefile practices

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,8 +1,7 @@
-# Download dataset 1
+# Download dataset December 2020
 # dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file('https://rgreminger.github.io/files/dataset1.csv','./data/dataset1/dataset1.csv')
+download.file('https://github.com/course-dprep/SickAirbnbPricesAcrossNetherlands/raw/main/Data/listings-12.20.csv.gz', './data/dataset1/dataset2020.csv.gz')
 
-# Download dataset 2
+# Download dataset December 2022
 # dir.create('./data/dataset2')  # Uncomment if need to create directory with R
-download.file('https://rgreminger.github.io/files/dataset2.csv','./data/dataset2/dataset2.csv')
-
+download.file('http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/data/listings.csv.gz', './data/dataset1/dataset2022.csv.gz')
--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -1,9 +1,9 @@
 # Load datasets into R 
-df1 <- read.csv("./gen/data-preparation/input/dataset1.csv")
-df2 <- read.csv("./gen/data-preparation/input/dataset2.csv")
+df2020 <- read.csv(gzfile("./gen/data-preparation/input/dataset2020.csv.gz"))
+df2022 <- read.csv(gzfile("./gen/data-preparation/input/dataset2022.csv.gz"))
 
 # Merge on id
-df_merged <- merge(df1,df2,by="id")
+df_merged <- rbind(df2020, df2022)
 
 # Save merged data
 save(df_merged,file="./gen/data-preparation/temp/data_merged.RData")
