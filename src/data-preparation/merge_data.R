fa52d963a15e66ce3ed58dc67c14296f37752e05
KleinJonasUVT <123603524+KleinJonasUVT@users.noreply.github.com>
2023-03-06 13:32:54 +0100

    Use of read_csv() instead of read.csv()

--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -1,6 +1,12 @@
+# Install the readr package
+install.packages("readr")
+
+# Load the readr package
+library(readr)
+
 # Load datasets into R 
-df2020 <- read.csv(gzfile("./gen/data-preparation/input/dataset2020.csv.gz"))
-df2022 <- read.csv(gzfile("./gen/data-preparation/input/dataset2022.csv.gz"))
+df2020 <- read_csv(gzfile("./gen/data-preparation/input/dataset2020.csv.gz"))
+df2022 <- read_csv(gzfile("./gen/data-preparation/input/dataset2022.csv.gz"))
 
 # Merge on id
 df_merged <- rbind(df2020, df2022)
