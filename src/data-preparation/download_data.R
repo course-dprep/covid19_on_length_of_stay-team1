cd8f439f8abc96e57c774016edb12298695e26b3
KleinJonasUVT <123603524+KleinJonasUVT@users.noreply.github.com>
2023-03-06 13:53:57 +0100

    Fixed relative directories

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,7 +1,5 @@
 # Download dataset December 2020
-# dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file('https://github.com/course-dprep/SickAirbnbPricesAcrossNetherlands/raw/main/Data/listings-12.20.csv.gz', './data/dataset1/dataset2020.csv.gz')
+download.file('https://github.com/course-dprep/SickAirbnbPricesAcrossNetherlands/raw/main/Data/listings-12.20.csv.gz', '../../data/dataset1/dataset2020.csv.gz')
 
 # Download dataset December 2022
-# dir.create('./data/dataset2')  # Uncomment if need to create directory with R
-download.file('http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/data/listings.csv.gz', './data/dataset1/dataset2022.csv.gz')
+download.file('http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/data/listings.csv.gz', '../../data/dataset1/dataset2022.csv.gz')
