1dfc375132bb07d1a801ebcf05b3cf2f45d15892
KleinJonasUVT <123603524+KleinJonasUVT@users.noreply.github.com>
2023-03-06 14:03:57 +0100

    fix relative directories

--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -1,15 +1,12 @@
-# Install the readr package
-install.packages("readr")
-
 # Load the readr package
 library(readr)
 
 # Load datasets into R 
-df2020 <- read_csv(gzfile("./gen/data-preparation/input/dataset2020.csv.gz"))
-df2022 <- read_csv(gzfile("./gen/data-preparation/input/dataset2022.csv.gz"))
+df2020 <- read_csv(gzfile("../../gen/data-preparation/input/dataset2020.csv.gz"))
+df2022 <- read_csv(gzfile("../../gen/data-preparation/input/dataset2022.csv.gz"))
 
 # Merge on id
 df_merged <- rbind(df2020, df2022)
 
 # Save merged data
-save(df_merged,file="./gen/data-preparation/temp/data_merged.RData")
+write_csv(df_merged, "../../gen/data-preparation/temp/data_merged.csv")
