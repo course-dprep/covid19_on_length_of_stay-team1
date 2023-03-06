438b22c8142093b140be9707d0e7fccd07015f28
Jonas Klein <jonasklein2001@gmail.com>
2023-03-06 14:13:28 +0100

    test initial steps of workflow

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,5 +1,5 @@
 # Download dataset December 2020
-download.file('https://github.com/course-dprep/SickAirbnbPricesAcrossNetherlands/raw/main/Data/listings-12.20.csv.gz', '../../data/dataset1/dataset2020.csv.gz')
+download.file('https://github.com/course-dprep/SickAirbnbPricesAcrossNetherlands/raw/main/Data/listings-12.20.csv.gz', '../../data/dataset2020.csv.gz')
 
 # Download dataset December 2022
-download.file('http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/data/listings.csv.gz', '../../data/dataset1/dataset2022.csv.gz')
+download.file('http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/data/listings.csv.gz', '../../data/dataset2022.csv.gz')
--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -2,11 +2,16 @@
 library(readr)
 
 # Load datasets into R 
-df2020 <- read_csv(gzfile("../../gen/data-preparation/input/dataset2020.csv.gz"))
-df2022 <- read_csv(gzfile("../../gen/data-preparation/input/dataset2022.csv.gz"))
+df2020 <- read_csv(gzfile("../../data/dataset2020.csv.gz"))
+df2022 <- read_csv(gzfile("../../data/dataset2022.csv.gz"))
 
 # Merge on id
-df_merged <- rbind(df2020, df2022)
+cols1 = colnames(df2020)
+cols2 = colnames(df2022)
+
+cols = intersect(cols1, cols2)
+
+df_merged <- rbind(df2020[, cols], df2022[, cols])
 
 # Save merged data
 write_csv(df_merged, "../../gen/data-preparation/temp/data_merged.csv")
