bced809f829f869d9694e24cd50930c177cb61fa
Jonas Klein <jonasklein2001@gmail.com>
2023-03-07 18:15:45 +0100

    Seperated the outliers removal part

---
+++ src/data-preparation/remove_outliers.R
@@ -0,0 +1,35 @@
+# Load cleaned data 
+library(readr)
+read_csv("../../gen/data-preparation/output/data_cleaned.csv")
+
+# Remove outliers for all relevant columns
+col_list <- c('price', 'minimum_nights') # create a list of column names
+
+# loop over the column list
+for (col_name in col_list) {
+  
+  # detect outliers of the current column
+  z_scores <- scale(df_cleaned[[col_name]])
+  outliers <- abs(z_scores) > 3
+  df_outliers <- df_cleaned[outliers,]
+  
+  # remove outliers of the current column
+  df_cleaned <- df_cleaned[!outliers,]
+  
+  # histogram of the current column values after removal of outliers
+  plot <- ggplot(df_cleaned, aes(x = .data[[col_name]])) + 
+    geom_histogram(binwidth = 1, fill = "steelblue", color = "black") + 
+    ggtitle(paste("Histogram of", col_name)) + 
+    xlab("Value") + 
+    ylab("Frequency")
+  
+  # print the plot
+  print(plot)
+  
+}
+
+# make sure outliers are removed for both 2020 and 2022
+listings_both_years_2 <- duplicated(df_cleaned$listing_url) | duplicated(df_cleaned$listing_url, fromLast = TRUE)
+df_cleaned <- subset(df_cleaned, listings_both_years_2)
+
+write_csv(df_cleaned,file="../../gen/data-preparation/output/data_no_outliers.csv")
