374cee0702ee3edc7be21ba87d20e04f0fcc9b9d
Jonas Klein <jonasklein2001@gmail.com>
2023-03-08 17:19:28 +0100

    added our most basic models

--- src/analysis/analyze.R
+++ src/analysis/analyze.R
@@ -1,11 +1,31 @@
 # load 
-load("./gen/analysis/input/data_cleaned.RData")
+read_csv("../../gen/data-preparation/output/data_no_outliers.csv")
 
-# Estimate model 1 
-m1 <- lm(V1 ~ V3 + V4,df_cleaned)
+# Inspecting means of minimum nights of stay before running regressions
+df_cleaned_2020 <- df_cleaned %>% filter(grepl("2020", df_cleaned$last_scraped))
+mean(df_cleaned_2020$minimum_nights)
+df_cleaned_2022 <- df_cleaned %>% filter(grepl("2022", df_cleaned$last_scraped))
+mean(df_cleaned_2022$minimum_nights)
 
-# Estimate model 2 
-m2 <- lm(V1 ~ V3 + V4 + V5 , df_cleaned)
+# most basic model
+m0 <- lm(minimum_nights ~ covid, df_cleaned)
+summary(m0)
+         
+# Independence (Marijn)
+
+# Homoskedasticity (Matthijs)
+
+# Normality (Jonas)
+
+# Linearity (Dianne)
+
+# Estimate simple model
+m1 <- lm(minimum_nights ~ covid + as.factor(neighbourhood_num) + as.factor(roomtype_num) + accommodates + price + instant_bookable, df_cleaned)
+summary(m1)
+
+# Multicollinearity (Matthijs)
+
+# Running the simple model again with possible changes (Jonas)
 
 # Save results
-save(m1,m2,file="./gen/analysis/output/model_results.RData")
+save(m1,file="../../gen/analysis/output/model_results.RData")
